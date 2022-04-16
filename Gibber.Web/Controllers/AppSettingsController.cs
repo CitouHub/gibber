cd84360d0ef9b7ffaf3a3797fb7d3b3c5403c959
CitouHub <218735@skane.se>
2022-04-16 18:19:09 +0200

    Cleaned the web project

---
+++ Gibber.Web/Controllers/AppSettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gibbler.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppSettingsController : ControllerBase
+    {
+        private readonly ILogger<AppSettingsController> _logger;
+
+        public AppSettingsController(ILogger<AppSettingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
