07519d334fbb9fabd6ab44031823fd518838c50f
CitouHub <218735@skane.se>
2022-05-01 23:04:21 +0200

    Version

--- gibbr.API/BackgroundService/DatabaseMigrationBackgroundService.cs
+++ gibbr.API/BackgroundService/DatabaseMigrationBackgroundService.cs
@@ -109,8 +109,8 @@ namespace gibbr.API.BackgroundService
             }
 
             //Insert the version, replace the '#' character
-            var versionLetters = typeof(Program)?.Assembly?.GetName()?.Version?.ToString().ToCharArray();
-            //var versionLetters = typeof(Program)?.GetReleaseTag()?.ToCharArray();
+            //var versionLetters = typeof(Program)?.Assembly?.GetName()?.Version?.ToString().ToCharArray();
+            var versionLetters = typeof(Program)?.GetReleaseTag()?.ToCharArray();
             if (versionLetters is not null && versionX is not null && versionY is not null)
             {
                 x = versionX.Value;
